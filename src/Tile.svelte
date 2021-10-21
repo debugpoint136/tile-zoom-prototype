8ae99439d92cbc37b58e386a8ee7a6f1a7419057
Deepak Purushotham <dpurushotham136@gmail.com>
2021-10-21 14:29:33 -0500

    comments

--- src/Tile.svelte
+++ src/Tile.svelte
@@ -17,43 +17,54 @@
   });
 
   function init() {
-    const svg = d3
-      .select(el)
-      .append('svg')
-      .attr('viewBox', [0, 0, width * 1.4, height * 1.4]);
+    const svg = d3 // create a d3 selection of an element
+      .select(el) // el variable holds the div where this whole visualization component gets injected
+      .append('svg') // insert an SVG element
+      .attr('viewBox', [0, 0, width * 1.4, height * 1.4]); // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/viewBox
 
     const tiler = d3Tile.tile().extent([
+      // d3.tile() generates coordinates x/y/z e.g. 1/1/2
       [0, 0],
       [width, height],
     ]);
 
-    const zoom = d3
+    // understanding the zoom part might be most complex - go through this to start with the basics : https://observablehq.com/@enjalot/d3-zoom-edumix
+    // Setup the zoom
+    const zoom = d3 // d3.zoom is for panning and zooming
       .zoom()
-      .scaleExtent([1 << 8, 1 << 22])
+      .scaleExtent([1 << 8, 1 << 22]) // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Left_shift, so 1 << 8 is 256 ; 1 << 22 is 4194304
       .extent([
         [0, 0],
-        [width, height],
+        [width, height], // the above matrix is 'mapped' to the screen shape
       ])
-      .on('zoom', (event) => zoomed(event.transform));
+      .on('zoom', (event) => zoomed(event.transform)); // attaching event listener
 
-    const tileGroup = svg
+    const tileGroup = svg //svg is like collections of groups of elements
       .append('g')
       .attr('pointer-events', 'none')
       .attr('font-family', 'var(--sans-serif)')
       .attr('font-size', 16);
 
-    let tile = tileGroup.selectAll('g');
+    let tile = tileGroup.selectAll('g'); // handle for tile
 
-    svg
-      .call(zoom)
-      .call(
-        zoom.transform,
-        d3.zoomIdentity.translate(width >> 1, height >> 1).scale(1 << 10)
-      );
+    // svg.call(zoom) sets up the mouse/touch listeners on the SVG element for the zoom
+
+    // this sets the initial zoom to the "identity" transform
+    // which is just scale of 1 and no translation in x or y.
+    // and then translate to
+
+    svg.call(zoom).call(
+      zoom.transform,
+      d3.zoomIdentity.translate(width >> 1, height >> 1).scale(1 << 10) // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Right_shift...so 3000 >> 1 is 1500
+    );
 
     function zoomed(transform) {
-      const tiles = tiler(transform);
+      // This is what actually changes what we see, the "transform"
+      const tiles = tiler(transform); // tiler function generates tiles, see above
       console.log(tiles);
+
+      // When we set translate() on an SVG element, we're moving around its coordinate system
+      // https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/transform#translate
 
       tileGroup.attr(
         'transform',
@@ -64,30 +75,34 @@
       );
 
       tile = tile
-        .data(tiles, (d) => d)
-        .join((enter) =>
-          enter
-            .append('g')
-            .attr(
-              'transform',
-              ([x, y]) => `translate(${x}, ${y}) scale(${1 / 256})`
-            )
-            .call((g) =>
-              g
-                .append('rect')
-                .attr('fill', (d) => d3.interpolateRainbow(hilbert(...d)))
-                .attr('fill-opacity', 0.5)
-                .attr('stroke', 'black')
-                .attr('width', 256)
-                .attr('height', 256)
-            )
-            .call((g) =>
-              g
-                .append('text')
-                .attr('x', '0.4em')
-                .attr('y', '1.2em')
-                .text((d) => d.join('/'))
-            )
+        .data(tiles, (d) => d) //attaching the generated tiles
+        .join(
+          (
+            enter // enter is a special term to say that new data is available
+          ) =>
+            enter
+              .append('g') // create a new group per item and transform it to it's position
+              .attr(
+                'transform',
+                ([x, y]) => `translate(${x}, ${y}) scale(${1 / 256})`
+              )
+              .call((g) =>
+                g
+                  .append('rect') // add rectangle under another <g></g> tag
+                  .attr('fill', (d) => d3.interpolateRainbow(hilbert(...d))) // colormap - https://corte.si/posts/code/hilbert/swatches/
+                  .attr('fill-opacity', 0.5)
+                  .attr('stroke', 'black')
+                  .attr('width', 256)
+                  .attr('height', 256)
+              )
+              .call(
+                (g) =>
+                  g
+                    .append('text')
+                    .attr('x', '0.4em')
+                    .attr('y', '1.2em')
+                    .text((d) => d.join('/')) // this creates the text on each tile e.g. 1/3/2
+              )
         );
     }
 
@@ -95,6 +110,7 @@
   }
 
   function hilbert(x, y, z) {
+    // don't worry about this colormap generating code
     let n = 1 << z,
       rx,
       ry,
